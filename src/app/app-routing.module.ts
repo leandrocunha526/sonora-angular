b1960f96befc83890cc11b34b5b9d5689c3f558f
Leandro Cunha <leandrocunha016@gmail.com>
2025-04-21 20:09:09 -0300

    feat: add user and city edit routes to the routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,18 +10,24 @@ import { CityComponent } from './city/city.component';
 import { ProductComponent } from './product/product.component';
 import { EditProductComponent } from './product/edit-product/edit-product.component';
 import { CityManagerComponent } from './city/city-manager/city-manager.component';
+import { UserEditComponent } from './user/user-edit/user-edit.component';
+import { EditCityComponent } from './city/edit-city/edit-city.component';
 
 const routes: Routes = [
   // Rotas públicas
+  // Rota de login e registro
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
 
-  // Rota protegida
+  // Rotas protegidas
+  // Rota do dashboard
   {
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [AuthGuard],
   },
+
+  // Rotas para produtos
 
   { path: 'product', component: ProductComponent, canActivate: [AuthGuard] },
   {
@@ -30,10 +36,18 @@ const routes: Routes = [
     canActivate: [AuthGuard],
   },
 
+  // Rotas para cidades
+
   { path: 'city', component: CityComponent, canActivate: [AuthGuard] },
   { path: 'cities', component: CityManagerComponent, canActivate: [AuthGuard] },
+  { path: 'cities/edit/:id', component: EditCityComponent, canActivate: [AuthGuard] },
+
+  // Rotas para usuários
 
   { path: 'user', component: UserComponent, canActivate: [AuthGuard] },
+  { path: 'users/edit/:id', component: UserEditComponent, canActivate: [AuthGuard] },
+
+  // Rota para o perfil do usuário
 
   { path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] },
 
