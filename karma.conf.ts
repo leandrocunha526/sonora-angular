5c4378394fccdf87afd3f92e5018689697578bc0
Leandro Cunha <leandrocunha016@gmail.com>
2025-04-22 16:22:21 -0300

    ci: update test configuration to use ChromeHeadlessNoSandbox for improved compatibility

--- karma.conf.ts
+++ karma.conf.ts
@@ -1,17 +1,17 @@
 import { Config, ConfigOptions } from 'karma';
 
-export default function (config: Config) {
+export default function (config: Config): void {
   const configuration: ConfigOptions = {
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
+        flags: ['--no-sandbox', '--disable-gpu']
       }
     },
+    singleRun: true,
     reporters: ['progress'],
-    singleRun: true
   };
 
   config.set(configuration);
